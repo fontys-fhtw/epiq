b524cbbceecf8184e8e79b2f234698822e0dedf0
Valentino-Dittmar <151268060+Valentino-Dittmar@users.noreply.github.com>
2024-09-30 13:44:23 +0200

    added a around the sign-up inputs to match the look of the sign-in

--- src/components/customer/SignUp.jsx
+++ src/components/customer/SignUp.jsx
@@ -49,128 +49,130 @@ const SignUp = () => {
         backgroundImage: "url('/background.webp')",
       }}
     >
-      <h2 className="mb-6 text-center text-2xl font-bold">Register</h2>
+      <div className="w-full max-w-md rounded-lg bg-white bg-opacity-25 p-8 shadow-md">
+        <h2 className="mb-6 text-center text-2xl font-bold">Sign Up</h2>
 
-      <Formik
-        initialValues={{
-          first_name: "",
-          last_name: "",
-          email: "",
-          password: "",
-        }}
-        validationSchema={schema}
-        onSubmit={mutate}
-      >
-        {({ errors }) => (
-          <Form>
-            {errors.server && (
-              <div className="mb-2 text-sm text-red-500">{errors.server}</div>
-            )}
+        <Formik
+          initialValues={{
+            first_name: "",
+            last_name: "",
+            email: "",
+            password: "",
+          }}
+          validationSchema={schema}
+          onSubmit={mutate}
+        >
+          {({ errors }) => (
+            <Form>
+              {errors.server && (
+                <div className="mb-2 text-sm text-red-500">{errors.server}</div>
+              )}
 
-            {/* First Name Field */}
-            <div className="mb-4">
-              <label
-                htmlFor="first_name"
-                className="block text-sm font-medium text-gray-700"
-              >
-                First Name
-              </label>
-              <Field
-                name="first_name"
-                type="text"
-                className="mt-1 block w-full rounded-md border border-gray-300 p-2 focus:border-indigo-500 focus:ring-indigo-500"
-              />
-              <ErrorMessage
-                name="first_name"
-                component="div"
-                className="mt-1 text-sm text-red-500"
-              />
-            </div>
-
-            {/* Last Name Field */}
-            <div className="mb-4">
-              <label
-                htmlFor="last_name"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Last Name
-              </label>
-              <Field
-                name="last_name"
-                type="text"
-                className="mt-1 block w-full rounded-md border border-gray-300 p-2 focus:border-indigo-500 focus:ring-indigo-500"
-              />
-              <ErrorMessage
-                name="last_name"
-                component="div"
-                className="mt-1 text-sm text-red-500"
-              />
-            </div>
-
-            {/* Email Field */}
-            <div className="mb-4">
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Email Address
-              </label>
-              <Field
-                name="email"
-                type="email"
-                className="mt-1 block w-full rounded-md border border-gray-300 p-2 focus:border-indigo-500 focus:ring-indigo-500"
-              />
-              <ErrorMessage
-                name="email"
-                component="div"
-                className="mt-1 text-sm text-red-500"
-              />
-            </div>
-            {/* Password Field with Eye Icon Inside the Input */}
-            <div className="mb-4">
-              <label
-                htmlFor="password"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Password
-              </label>
-              <div className="relative">
+              {/* First Name Field */}
+              <div className="mb-4">
+                <label
+                  htmlFor="first_name"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  First Name
+                </label>
                 <Field
-                  name="password"
-                  type={showPassword ? "text" : "password"} // Toggle between text and password
-                  className="mt-1 block w-full rounded-md border border-gray-300 p-2 pr-10 focus:border-indigo-500 focus:ring-indigo-500"
+                  name="first_name"
+                  type="text"
+                  className="mt-1 block w-full rounded-md border border-gray-300 p-2 focus:border-indigo-500 focus:ring-indigo-500"
                 />
                 <ErrorMessage
-                  name="password"
+                  name="first_name"
                   component="div"
                   className="mt-1 text-sm text-red-500"
                 />
-                {/* Eye icon to toggle visibility */}
-                <button
-                  type="button"
-                  onClick={togglePasswordVisibility}
-                  className="absolute inset-y-0 right-0 flex items-center pr-3 focus:outline-none"
-                >
-                  {showPassword ? (
-                    <EyeSlashIcon className="size-5 text-gray-500" />
-                  ) : (
-                    <EyeIcon className="size-5 text-gray-500" />
-                  )}
-                </button>
               </div>
-            </div>
 
-            {/* Submit Button */}
-            <button
-              type="submit"
-              disabled={isPending}
-              className="w-full rounded-md bg-indigo-600 p-2 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-            >
-              {isPending ? "Registering..." : "Register"}
-            </button>
-          </Form>
-        )}
-      </Formik>
+              {/* Last Name Field */}
+              <div className="mb-4">
+                <label
+                  htmlFor="last_name"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Last Name
+                </label>
+                <Field
+                  name="last_name"
+                  type="text"
+                  className="mt-1 block w-full rounded-md border border-gray-300 p-2 focus:border-indigo-500 focus:ring-indigo-500"
+                />
+                <ErrorMessage
+                  name="last_name"
+                  component="div"
+                  className="mt-1 text-sm text-red-500"
+                />
+              </div>
+
+              {/* Email Field */}
+              <div className="mb-4">
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Email Address
+                </label>
+                <Field
+                  name="email"
+                  type="email"
+                  className="mt-1 block w-full rounded-md border border-gray-300 p-2 focus:border-indigo-500 focus:ring-indigo-500"
+                />
+                <ErrorMessage
+                  name="email"
+                  component="div"
+                  className="mt-1 text-sm text-red-500"
+                />
+              </div>
+              {/* Password Field with Eye Icon Inside the Input */}
+              <div className="mb-4">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Password
+                </label>
+                <div className="relative">
+                  <Field
+                    name="password"
+                    type={showPassword ? "text" : "password"} // Toggle between text and password
+                    className="mt-1 block w-full rounded-md border border-gray-300 p-2 pr-10 focus:border-indigo-500 focus:ring-indigo-500"
+                  />
+                  <ErrorMessage
+                    name="password"
+                    component="div"
+                    className="mt-1 text-sm text-red-500"
+                  />
+                  {/* Eye icon to toggle visibility */}
+                  <button
+                    type="button"
+                    onClick={togglePasswordVisibility}
+                    className="absolute inset-y-0 right-0 flex items-center pr-3 focus:outline-none"
+                  >
+                    {showPassword ? (
+                      <EyeSlashIcon className="size-5 text-gray-500" />
+                    ) : (
+                      <EyeIcon className="size-5 text-gray-500" />
+                    )}
+                  </button>
+                </div>
+              </div>
+
+              {/* Submit Button */}
+              <button
+                type="submit"
+                disabled={isPending}
+                className="w-full rounded-md bg-indigo-600 p-2 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              >
+                {isPending ? "Registering..." : "Register"}
+              </button>
+            </Form>
+          )}
+        </Formik>
+      </div>
     </div>
   );
 };
