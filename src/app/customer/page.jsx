43c1d93f48c2e62575b8ee361ea08dcfe0264795
Ruslan Kotliarenko <ruslan.kotliarenko@dxc.com>
2024-10-03 21:16:18 +0200

    Feature: customer profile with log-out functionality

--- src/app/customer/page.jsx
+++ src/app/customer/page.jsx
@@ -2,12 +2,18 @@ import Link from "next/link";
 
 export default function CustomerPage() {
   return (
-    <div className="p-4">
+    <div className="flex flex-col gap-5 p-4">
       <Link
         className="rounded bg-red-500 px-4 py-2 text-white transition hover:bg-red-600"
         href="/customer/restaurant-menu"
       >
-        Menu page
+        Restaurant Menu
+      </Link>
+      <Link
+        className="rounded bg-blue-500 px-4 py-2 text-white transition hover:bg-blue-600"
+        href="/customer/profile"
+      >
+        Customer Profile
       </Link>
     </div>
   );
