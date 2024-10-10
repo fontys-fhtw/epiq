75e77286bb0fe63b599593e1882c1816a89ae975
Marco Auenhammer <marco.auenhammer@gmail.com>
2024-10-10 14:36:37 +0200

    added callback route

---
+++ src/app/api/auth/callback/route.js
@@ -0,0 +1,25 @@
+// The client you created from the Server-Side Auth instructions
+import { createSupabaseServerClient } from "@src/utils/supabase/serverClient";
+import { NextResponse } from "next/server";
+
+export async function GET(request) {
+  const { searchParams, origin } = new URL(request.url);
+  const code = searchParams.get("code");
+  // if "next" is in param, use it as the redirect URL
+  const next = searchParams.get("next") ?? "/";
+
+  if (code) {
+    const supabase = createSupabaseServerClient();
+    const { error } = await supabase.auth.exchangeCodeForSession(code);
+    if (!error) {
+      const forwardedHost = request.headers.get("x-forwarded-host"); // original origin before load balancer
+      if (forwardedHost) {
+        return NextResponse.redirect(`https://${forwardedHost}${next}`);
+      }
+      return NextResponse.redirect(`${origin}${next}`);
+    }
+  }
+
+  // return the user to an error page with instructions
+  return NextResponse.redirect(`${origin}/auth/auth-code-error`);
+}
