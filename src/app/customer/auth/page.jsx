e2a7856346868faa65ac32ce98d4aaa08f09a51c
Ruslan Kotliarenko <ruslankot101@gmail.com>
2024-10-31 14:00:27 +0100

    Testing oauth redirect in prod

--- src/app/customer/auth/page.jsx
+++ src/app/customer/auth/page.jsx
@@ -3,6 +3,7 @@
 import { authUser } from "@src/queries/customer";
 import createSupabaseBrowserClient from "@src/utils/supabase/browserClient";
 import { useMutation } from "@tanstack/react-query";
+import { getURL } from "next/dist/shared/lib/utils";
 import { useSearchParams } from "next/navigation";
 import { Suspense, useEffect } from "react";
 
@@ -11,7 +12,12 @@ function AuthComponent() {
   const searchParams = useSearchParams();
 
   const { mutate, isError, error } = useMutation({
-    mutationFn: () => authUser(supabase, searchParams.get("referrerId")),
+    mutationFn: () => {
+      console.log(
+        `Redirecting to: ${getURL().api}auth/callback/${searchParams.get("referrerId")}`,
+      );
+      // authUser(supabase, searchParams.get("referrerId"))
+    },
   });
 
   useEffect(() => {
