820728cd04091ac3c65b8b09ebdbfd35ebcb92b9
Ruslan Kotliarenko <ruslankot101@gmail.com>
2024-11-25 10:04:38 +0100

    Fix: invalid tailwind classname

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -27,7 +27,7 @@ export default function Home() {
         <Link
           key={subApp.id}
           href={subApp.href}
-          className={`flex h-1/2 w-full items-center justify-center border-b border-dark text-4xl font-bold transition-all duration-300 last:border-b-0 md:h-full md:w-1/2 md:border-r last:md:border-r-0${subApp.textColor} ${subApp.bgColor} ${subApp.hoverColor} hover:scale-105`}
+          className={`flex h-1/2 w-full items-center justify-center border-b border-dark text-4xl font-bold transition-all duration-300 last:border-b-0 md:h-full md:w-1/2 md:border-r last:md:border-r-0 ${subApp.textColor} ${subApp.bgColor} ${subApp.hoverColor} hover:scale-105`}
         >
           <div className="text-center">{subApp.name}</div>
         </Link>
