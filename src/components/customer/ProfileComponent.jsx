0dc9d1cb99a04c6ed4a5b542a0a7ca1aa0c86094
Marco Auenhammer <marco.auenhammer@gmail.com>
2024-12-08 21:39:43 +0100

    added notifications ui element

--- src/components/customer/ProfileComponent.jsx
+++ src/components/customer/ProfileComponent.jsx
@@ -135,6 +135,14 @@ export default function CustomerProfile() {
       </div>
 
       <div>
+        <label className="inline-flex cursor-pointer items-center">
+          <input type="checkbox" value="" className="peer sr-only" />
+          <div className="peer relative h-6 w-11 rounded-full bg-gray-200 after:absolute after:start-[2px] after:top-[2px] after:size-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-gold peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rtl:peer-checked:after:-translate-x-full dark:border-gray-600 dark:bg-gray-700 dark:peer-focus:ring-blue-800" />
+          <span className="ms-3">Enable notifications</span>
+        </label>
+      </div>
+
+      <div>
         <ActionButton onClick={handleShare} className="rounded-lg text-lg">
           📤 Get €10 with a Friend
         </ActionButton>
